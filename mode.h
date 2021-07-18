ed21731498788ab22666d1cc808907c101243ac6
jkwok678 <jkwok678@gmail.com>
2021-07-18 12:29:24 +0100

    Added 2 enums
    ElementChosen will be used for choosing an element to place.
    Mode will be for whatever mode the program is in, and will determine what features, the user can use.

---
+++ mode.h
@@ -0,0 +1,17 @@
+#ifndef MODE_H
+#define MODE_H
+
+#endif // MODE_H
+
+/**
+ * @brief The Mode enum
+ *
+ * This enum is for the Window class to use mainly.
+ * It contains all of the modes, that are possible in the program.
+ */
+
+enum class Mode
+{
+	NONE, ADDREMOVETRACK
+
+};
