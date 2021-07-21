4d6ae29288aeb4d8847744b4664048fd82d9f793
jkwok678 <jkwok678@gmail.com>
2021-07-21 13:20:04 +0100

    Added Window class
    Initial GUI

--- window.cpp
+++ window.cpp
@@ -1,11 +1,112 @@
 #include "window.h"
 
-Window::Window(QWidget *parent)
-	: QWidget(parent)
+Window::Window()
 {
+	Canvas *centralWidget = new Canvas;
+	createMenuBar();
+	createOverallMenu();
+	mode = Mode::NONE;
+	BorderLayout *layout = new BorderLayout;
+	layout->addWidget(menuBar,BorderLayout::North);
+	layout->addWidget(top1Menu,BorderLayout::North);
+	layout->addWidget(allMenus,BorderLayout::North);
+	layout->addWidget(centralWidget, BorderLayout::Center);
+	setLayout(layout);
 }
 
 Window::~Window()
 {
 }
 
+void Window::openElementMenu()
+{
+	if (allMenus->currentIndex() != 1)
+	{
+		allMenus->setCurrentIndex(1);
+		mode = Mode::ADDREMOVETRACK;
+	}
+	else
+	{
+		allMenus->setCurrentIndex(0);
+		mode = Mode::NONE;
+	}
+
+}
+
+void Window::chooseStraightH()
+{
+	if (mode==Mode::ADDREMOVETRACK && elementChosenToPlace != ElementChosen::STRAIGHTH)
+	{
+		elementChosenToPlace = ElementChosen::STRAIGHTH;
+	}
+	else
+	{
+		elementChosenToPlace = ElementChosen::NONE;
+	}
+}
+
+void Window::createMenuBar()
+{
+	menuBar = new QMenuBar();
+	fileMenu = new QMenu("File");
+	modeMenu = new QMenu("Mode");
+	menuBar->addMenu(fileMenu);
+	menuBar->addMenu(modeMenu);
+}
+
+void Window::createOverallMenu()
+{
+	top1Menu = new QWidget();
+	buildModifyMenu1 = new QHBoxLayout;
+	QPalette pal2 = palette();
+	pal2.setColor(QPalette::Window, Qt::red);
+	top1Menu->setAutoFillBackground(true);
+	top1Menu->setPalette(pal2);
+	top1Menu->setLayout(buildModifyMenu1);
+
+	createBuildModifyMenu();
+
+
+	allMenus = new QStackedWidget;
+	createElementMenu();
+	allMenus->addWidget(new QWidget());
+	allMenus->addWidget(elementMenu);
+	allMenus->setCurrentIndex(0);
+
+}
+void Window::createBuildModifyMenu()
+{
+	elementMenuButton = new QToolButton();
+	elementMenuButton->setMaximumSize(QSize(32,32));
+	openElementMenuAct = new QAction;
+	elementMenuButton->setDefaultAction(openElementMenuAct);
+	connect(openElementMenuAct,&QAction::triggered,this,&Window::openElementMenu);
+	elementMenuIcon = new QIcon(":/icons/icons/buildMenuIcon.png");
+	elementMenuButton->setIcon(*elementMenuIcon);
+	buildModifyMenu1->addWidget(elementMenuButton);
+
+}
+
+void Window::createElementMenu()
+{
+	elementMenu = new QWidget;
+	QPalette pal = palette();
+	pal.setColor(QPalette::Window, Qt::green);
+	elementMenu->setAutoFillBackground(true);
+	elementMenu->setPalette(pal);
+	elementMenuLayout = new QGridLayout;
+	elementMenu->setLayout(elementMenuLayout);
+	createElementBlock1();
+}
+
+void Window::createElementBlock1()
+{
+	straightHButton = new QToolButton();
+	straightHButton->setMaximumSize(QSize(32, 32));
+	chooseStraightHAct = new QAction();
+	straightHButton->setDefaultAction(chooseStraightHAct);
+	connect(chooseStraightHAct, &QAction::triggered, this, &Window::chooseStraightH);
+	straightHIcon = new QIcon(":/graphics/graphics/straightH.png");
+	straightHButton->setIcon(*straightHIcon);
+	elementMenuLayout->addWidget(straightHButton, 0, 0);
+}
--- window.h
+++ window.h
@@ -2,13 +2,105 @@
 #define WINDOW_H
 
 #include <QWidget>
-
+#include <QWidget>
+#include <QPushButton>
+#include <QtCore>
+#include <QVBoxLayout>
+#include <QStackedWidget>
+#include <QMenuBar>
+#include <QToolButton>
+#include <QStatusBar>
+#include <QLabel>
+#include <QFontDialog>
+#include <string>
+#include <iostream>
+#include <QSizePolicy>
+#include "borderlayout.h"
+#include "canvas.h"
+#include "mode.h"
+#include "elementChosen.h"
+/**
+ * @brief The Window class for making the entire GUI for the program.
+ *
+ * @author Jonathan Kwok
+ * @version 0.1
+ */
 class Window : public QWidget
 {
 	Q_OBJECT
 
 public:
-	Window(QWidget *parent = nullptr);
+	/**
+	 * @brief A constructor for a Window object that extends the QWidget.
+	 * Create a window object that includes the different parts needed to make the program function like the Canvas and the BorderLayout.
+	 */
+	Window();
 	~Window();
+
+private slots:
+	/**
+	 * @brief A slot to open the element menu to place elements on screen.
+	 */
+	void openElementMenu();
+
+	/**
+	 * @brief A slot to select StraightH tracks to place on screen.
+	 */
+	void chooseStraightH();
+
+
+
+private:
+	/**
+	 * @brief A method to create the menubar that is at the top of the screen.
+	 *
+	 * Currently only addes a File and Mode menu.
+	 * @version 0.1
+	 */
+	void createMenuBar();
+
+	/**
+	 * @brief A method to create the overall menu of the program.
+	 */
+	void createOverallMenu();
+
+	/**
+	 * @brief A method to create the menu that is shown after the Build/Modify Railway is pressed in the menubar.
+	 */
+	void createBuildModifyMenu();
+
+	/**
+	 * @brief A method to create the menu where the user can choose track type.
+	 */
+	void createElementMenu();
+
+	/**
+	 * @brief A method to create the part 1 of the element buttons where the user can choose track type.
+	 */
+	void createElementBlock1();
+
+	QMenuBar *menuBar;
+	QMenu *fileMenu;
+	QMenu *modeMenu;
+
+
+	Mode mode;
+	ElementChosen elementChosenToPlace;
+	QWidget *top1Menu;
+
+	QHBoxLayout *buildModifyMenu1;
+	QToolButton *elementMenuButton;
+	QIcon   *elementMenuIcon;
+	QAction *openElementMenuAct;
+
+	QStackedWidget *allMenus;
+
+	QWidget *elementMenu;
+	QGridLayout* elementMenuLayout;
+
+	QToolButton *straightHButton;
+	QIcon *straightHIcon;
+	QAction *chooseStraightHAct;
+
 };
 #endif // WINDOW_H
